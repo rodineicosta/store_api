c08a6362bdb64dfc677cc2b6b273d04f9430c35f
rodineicosta <rodineicosta@physistec.com.br>
2025-08-17 00:47:12 -0300

    refactor: minimal changes

--- store/models/base.py
+++ store/models/base.py
@@ -1,15 +1,16 @@
-from datetime import datetime
+import uuid
+from datetime import datetime, timezone
 from decimal import Decimal
 from typing import Any
-import uuid
+
 from bson import Decimal128
 from pydantic import UUID4, BaseModel, Field, model_serializer
 
 
 class CreateBaseModel(BaseModel):
     id: UUID4 = Field(default_factory=uuid.uuid4)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
     @model_serializer
     def set_model(self) -> dict[str, Any]:
